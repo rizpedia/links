51d99f298df3b4b898a35686d5e2f544c9c5333f
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-06-26 20:57:46 +0700

    update gede

--- data/links.js
+++ data/links.js
@@ -1,46 +1,85 @@
 const links = [
+  {
+    id: 1,
+    name: "OVM",
+    about: "Dashboard OpenMediaVault NAS",
+    url: "https://server.rizpedia.my.id",
+    img: require("../public/assets/omv.svg"),
+  },
+  {
+    id: 2,
+    name: "Tunnel",
+    about: "Panel Management Akun Tunneling",
+    url: "https://tunnel.rizpedia.my.id/dashboard/",
+    img: require("../public/assets/website.webp"),
+  },
+  {
+    id: 3,
+    name: "YACD",
+    about: "YACD Panel Management Clash",
+    url: "https://yacd.rizpedia.my.id/ui/",
+    img: require("../public/assets/yacd.svg"),
+  },
+  {
+    id: 4,
+    name: "Portainer",
+    about: "Management Container",
+    url: "https://portainer.rizpedia.my.id",
+    img: require("../public/assets/portainer.svg"),
+  },
+  {
+    id: 5,
+    name: "NAS",
+    about: "Filerun NAS | Personal Cloud",
+    url: "https://cloud.rizpedia.my.id",
+    img: require("../public/assets/filerun.svg"),
+  },
+  {
+    id: 6,
+    name: "Emby",
+    about: "Website streaming pribadi",
+    url: "https://emby.rizpedia.my.id",
+    img: require("../public/assets/emby.svg"),
+  },
+  {
+    id: 7,
+    name: "Terminal",
+    about: "TTYD terminal",
+    url: "https://ttyd.rizpedia.my.id",
+    img: require("../public/assets/terminal.svg"),
+  },
+  {
+    id: 8,
+    name: "Router",
+    about: "Web UI Router",
+    url: "https://router.rizpedia.my.id",
+    img: require("../public/assets/website.webp"),
+  },
+<<<<<<< HEAD
+  {
+    id: 10,
+    name: "Web Development Server",
+    about: "Disinilah saya meletakan kodingan",
+    url: "https://phpserver.rizpedia.my.id",
+    img: require("../public/assets/website.webp"),
+  },
+  {
+    id: 11,
+    name: "PhpMyAdmin",
+    about: "Dashboard phpmyadmin",
+    url: "https://phpmyadmin.rizpedia.my.id",
+    img: require("../public/assets/phpmyadmin.jpg"),
+  },
+
+=======
     {
-        id: 1,
-        name: "Website",
-        about: "Look at my work!",
-        url: "https://ssahibsingh.github.io",
-        img: require('../public/assets/website.webp')
-    },
-    {
-        id: 2,
-        name: "Github",
-        about: "@ssahibsingh | 🏡 to all my projects",
-        url: "https://github.com/ssahibsingh",
-        img: require('../public/assets/github.webp')
-    },
-    {
-        id: 3,
-        name: "LinkedIn",
-        about: "@ssahibsingh | Know more about my profession",
-        url: "https://www.linkedin.com/in/ssahibsingh/",
-        img: require('../public/assets/linkedin.webp')
-    },
-    {
-        id: 4,
-        name: "Twitter",
-        about: "@ssahibsingh_ | 😜",
-        url: "https://twitter.com/ssahibsingh_",
-        img: require('../public/assets/twitter.webp')
-    },
-    {
-        id: 5,
-        name: "Instagram",
-        about: "@ssahibsingh_ | Shots of my Life",
-        url: "https://www.instagram.com/ssahibsingh_/",
-        img: require('../public/assets/instagram.webp')
-    },
-    {
-        id: 6,
-        name: "Telegram",
-        about: "@cexnone | Chat with me instantly",
-        url: "https://t.me/cexnone",
-        img: require('../public/assets/telegram.webp')
-    },
+    id: 9,
+    name: "Personal Blog",
+    about: "Blog dengan isi yang random untuk berbagi informasi",
+    url: "https://blog.rizpedia.my.id",
+    img: require("../public/assets/website.webp"),
+  },
+>>>>>>> 64199a6670d86ba4131c95950405f4c65600f550
 ];
 
 export default links;
--- pages/index.js
+++ pages/index.js
@@ -9,33 +9,33 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sahib Singh&apos;s Links</title>
-        <meta name="title" content="Sahib Singh's Links" />
-        <meta name="author" content="Sahib Singh" />
+        <title>Rizkirmdhn Links</title>
+        <meta name="title" content="Rizkirmdhn Links" />
+        <meta name="author" content="Rizkirmdhn" />
         <meta
           name="description"
-          content="Sahib Singh's Links"
+          content="Rizkirmdhn Links"
         />
         <meta
           name="keywords"
-          content="Sahib Singh, Sahib Singh's Links, Sahib Singh's Linktree, Sahib, Soorme, Sardar Sahib Singh, Sahib Singh Khanna, Sahib Singh Github, Sahib Singh LinkedIn, Sahib Singh Gaming Geeks, ssahibsingh, Gaming Geeks, Soorme.com, Giigaa.com "
+          content="Rizkirmdhn, Rizpedia, NAS Rizkirmdhn, Link Tree Rizkirmdhn"
         />
-        <link rel="canonical" href="https://ssahibsingh.github.io/links" />
+        <link rel="canonical" href="https://rizpedia.my.id" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ssahibsingh.github.io/links" />
-        <meta property="og:title" content="Sahib Singh" />
+        <meta property="og:url" content="https://rizpedia.my.id" />
+        <meta property="og:title" content="Rizkirmdhn" />
         <meta
           property="og:description"
-          content="Sahib Singh's Links"
+          content="Rizkirmdhn Links"
         />
         <meta property="og:image" content="./website.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://ssahibsingh.github.io/links" />
-        <meta property="twitter:title" content="Sahib Singh" />
+        <meta property="twitter:url" content="https://rizpedia.my.id" />
+        <meta property="twitter:title" content="Rizkirmdhn" />
         <meta
           property="twitter:description"
-          content="Sahib Singh's Links"
+          content="Rizkirmdhn Links"
         />
         <meta property="twitter:image" content="./website.png" />
       </Head>
@@ -45,7 +45,7 @@ export default function Home() {
           <div className="d-flex justify-content-center align-items-center">
             <Image src={logo} className="logo img-fluid mx-2" alt="Logo" />
             <Link className="navbar-brand" href="/">
-              Sahib Singh.
+              Rizkirmdhn
             </Link>
           </div>
           {/* <button className="btn navbar-btn">Hello</button> */}
@@ -57,9 +57,9 @@ export default function Home() {
           <div className="row d-flex justify-content-center">
             {links.map((item) => {
               return (
-                <div key={item.id} className="col-md-4 col-6 my-3">
+                <div key={item.id} className="col-md-4  my-3">
                   <div className="cards-wrap">
-                    <Link href={item.url} rel="noreferrer" target="_blank">
+                    <Link href={item.url} rel="noreferrer">
                       <div className="cards p-3 d-flex justify-content-center">
                         <div className="cards-content">
                           <Image
@@ -79,13 +79,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-
-      {/* <footer className="my-2">
-        <p className="text-center">
-          Made with ❤️ by{" "}
-          <Link href="https://ssahibsingh.github.io">Sahib Singh</Link>
-        </p>
-      </footer> */}
     </>
   );
 }
